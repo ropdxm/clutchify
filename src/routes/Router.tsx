d37f6ca12db75452d7cf26598991dff81754cfbf
Dulat <ropdxm@gmail.com>
2023-08-03 03:40:11 +0600

    courses page, language switching

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,26 +1,15 @@
 import { paths } from "@utils/paths";
 import { lazy, ReactElement, Suspense, useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ChooseActivityPage from "./ChooseActivityPage/ChooseActivityPage";
 import LandingPage from "./LandingPage/LandingPage";
 import LoginPage from "./LoginPage/LoginPage";
 import Protected from "./Protected/Protected";
-import QuizDifficulties from "./QuizPage/QuizDifficulties/QuizDifficulties";
-import QuizPage from "./QuizPage/QuizPage";
 import RegistrationPage from "./RegistrationPage/RegistrationPage";
 import ResetPasswordPage from "./ResetPasswordPage/ResetPasswordPage";
 
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebase";
-
-const QuizQuestions = lazy(
-  () => import("./QuizPage/QuizQuestions/QuizQuestions")
-);
-
-const QuizLeaderboard = lazy(
-  () => import("./QuizPage/QuizLeaderboard/QuizLeaderboard")
-);
-const VocabularyPage = lazy(() => import("./VocabularyPage/VocabularyPage"));
+import Courses from "./Courses/Courses";
 
 export const Router = (): ReactElement => {
   useEffect(()=>{
@@ -46,34 +35,7 @@ export const Router = (): ReactElement => {
       <Routes>
         <Route element={<LandingPage />} path={paths.root} />
         <Route element={<Protected />} path={paths.app}>
-          <Route element={<ChooseActivityPage />} index />
-          <Route element={<QuizPage />} path={paths.quiz}>
-            <Route element={<QuizDifficulties />} index />
-            <Route
-              element={
-                <Suspense fallback={null}>
-                  <QuizLeaderboard />
-                </Suspense>
-              }
-              path={paths.quizLeaderboard}
-            />
-            <Route
-              element={
-                <Suspense fallback={null}>
-                  <QuizQuestions />
-                </Suspense>
-              }
-              path={paths.quizQuestions}
-            />
-          </Route>
-          <Route
-            element={
-              <Suspense fallback={null}>
-                <VocabularyPage />
-              </Suspense>
-            }
-            path={paths.vocabulary}
-          />
+          <Route element={<Courses />} index />
         </Route>
         <Route element={<LoginPage />} path={paths.login} />
         <Route element={<RegistrationPage />} path={paths.register} />
