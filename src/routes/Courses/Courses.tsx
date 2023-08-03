549955b7acaa22bc4dfab0e14c54c3e65310d0d6
Dulat <ropdxm@gmail.com>
2023-08-03 22:38:43 +0600

    course bug fix

--- src/routes/Courses/Courses.tsx
+++ src/routes/Courses/Courses.tsx
@@ -10,9 +10,11 @@ const Courses = () => {
       <Navbar />
       <h1 className='course-title-1'>Our Courses</h1>
       <section className='course-catalogue'>
-        <CourseCard title="How to fill Common App" img={exImg} owner="Duke University" ownerImg={dukeImg} courseLink='/app/quiz' />
+        <CourseCard title="How to fill Common App" img={exImg} owner="Duke University" ownerImg={dukeImg} courseLink='https://www.coursera.org/articles/applying-for-college' />
         
-        
+        <CourseCard title="How to fill Common App" img={exImg} owner="Duke University" ownerImg={dukeImg} courseLink='https://www.coursera.org/articles/applying-for-college' />
+        <CourseCard title="How to fill Common App" img={exImg} owner="Duke University" ownerImg={dukeImg} courseLink='https://www.coursera.org/articles/applying-for-college' />
+        <CourseCard title="How to fill Common App" img={exImg} owner="Duke University" ownerImg={dukeImg} courseLink='https://www.coursera.org/articles/applying-for-college' />
 
       </section>
     </main>
